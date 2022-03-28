b49c6eb0450b2149c5e7cdc31a33389471ec048f
tanhank2k <71895972+tanhank2k@users.noreply.github.com>
2022-03-29 00:32:39 +0700

    clean code v3

--- src/slag_program.java
+++ src/slag_program.java
@@ -10,12 +10,12 @@ public class slag_program {
 		String fileName = "./src/data/Slag.txt";
 		HashMap<String, String> data = ulti.ReadFileData(fileName);
 		List<String> history = ulti.readHistory("./src/history/history.txt");
-		
+
 		boolean condition = true;
-		
-		while(condition) {
+
+		while (condition) {
 			Scanner sc = new Scanner(System.in);
-		
+
 			System.out.println("-------------------Program SLANG-WORD!!!!------------------------");
 			System.out.println("Choose a feature: ");
 			System.out.println("1. Search by Slang-Word.");
@@ -26,204 +26,195 @@ public class slag_program {
 			System.out.println("6. Delete a Slang-Word.");
 			System.out.println("7. Reset all data.");
 			System.out.println("8. Random a Slang-Word.");
-			System.out.println("9. Funny Question: Show a random Slang-word, choose right the definition match Slang-word.");
-			System.out.println("10. Funny Question: Show a random Definition, choose right the slang-word match Slang-word.");
-			System.out.println("11. Exit the program!!");	
+			System.out.println(
+					"9. Funny Question: Show a random Slang-word, choose right the definition match Slang-word.");
+			System.out.println(
+					"10. Funny Question: Show a random Definition, choose right the slang-word match Slang-word.");
+			System.out.println("11. Exit the program!!");
 			System.out.println("Choose the feature: Enter 1 to 10!");
 			System.out.println("Your choice: ");
-		
+
 			String choice = sc.next();
 			sc.nextLine();
 			boolean condition_feature = true;
-		
-			switch(choice) {
+
+			switch (choice) {
 				case "1":
 					System.out.println("------------------Search by Slang-Word--------------------------");
-					while(condition_feature) {
-						
+					while (condition_feature) {
+
 						System.out.print("Enter Slang-word: ");
 						String slang = sc.nextLine();
 						String[] result = ulti.findDefinition(data, slang);
-				
+
 						history.add(slang);
 						ulti.writeHistory(history);
-					
-				
-						if(result.length>0) {
-							System.out.print("Definition \""+slang +  "\": ");
-							for(int i=0; i<result.length; i++) {
-								System.out.print(result[i]+"\t");
+
+						if (result.length > 0) {
+							System.out.print("Definition \"" + slang + "\": ");
+							for (int i = 0; i < result.length; i++) {
+								System.out.print(result[i] + "\t");
 							}
+						} else {
+							System.out.println("\"" + slang + "\" no exist in data");
 						}
-						else {
-							System.out.println("\""+slang + "\" no exist in data");
-						}
-						//Exit condition
-						if(ulti.exit("Search by Slang-Word")) {
+						// Exit condition
+						if (ulti.exit("Search by Slang-Word")) {
 							condition_feature = false;
 						}
 					}
 					break;
-				
+
 				case "2":
-				
 					System.out.println("------------------Search by Definition--------------------------");
-				
-					while(condition_feature) {
-					
+					while (condition_feature) {
+
 						System.out.print("Enter Definition: ");
 						String definition = sc.nextLine();
 						Set<String> key_result = ulti.getKeyByValue(data, definition);
 						String[] arrKey = new String[key_result.size()];
 						key_result.toArray(arrKey);
-				
-						if(key_result.size()>0) {
-							System.out.println("All Slang-word in definition has contain \""+definition +  "\": ");
-							for(int i=0; i<arrKey.length; i++) {
-								System.out.print("\"" + arrKey[i]+"\" ");
+
+						if (key_result.size() > 0) {
+							System.out.println("All Slang-word in definition has contain \"" + definition + "\": ");
+							for (int i = 0; i < arrKey.length; i++) {
+								System.out.print("\"" + arrKey[i] + "\" ");
 							}
+						} else {
+							System.out.println("\"" + definition + "\" no exist in data");
 						}
-						else {
-							System.out.println("\""+definition + "\" no exist in data");
-						}
-						//Exit condition
-						if(ulti.exit("Search by Definition")) {
+						// Exit condition
+						if (ulti.exit("Search by Definition")) {
 							condition_feature = false;
 						}
 					}
 					break;
-				
-				case "3"://Show history
+
+				case "3":// Show history
 					System.out.println("------------------Show history-------------------------------");
-					while(condition_feature) {
-						for(int i=0; i<history.size();i++) {
+					while (condition_feature) {
+						for (int i = 0; i < history.size(); i++) {
 							System.out.println(history.get(i));
 						}
-					
-						//Exit condition
-						if(ulti.exit("Show history")) {
+
+						// Exit condition
+						if (ulti.exit("Show history")) {
 							condition_feature = false;
 						}
 					}
 					break;
-				case "4"://Add Slang-word
+				case "4":// Add Slang-word
 					System.out.println("------------------Add new Slang-Word--------------------------");
-					while(condition_feature) {
-					
+					while (condition_feature) {
 						System.out.print("Enter new slang word: ");
 						String newSlang = sc.nextLine();
-
 						System.out.print("Enter new definition: ");
 						String newDefine = sc.nextLine();
-				
+
 						ulti.addSlangWord(data, newSlang, newDefine);
-				
-						//Exit condition
-						if(ulti.exit("Add new Slang-Word")) {
+
+						// Exit condition
+						if (ulti.exit("Add new Slang-Word")) {
 							condition_feature = false;
 						}
 						ulti.WriteFileData(data);
 					}
 					break;
-				case "5"://Edit Slang-word
+				case "5":// Edit Slang-word
 					System.out.println("------------------Edit a Slang-Word--------------------------");
-					while(condition_feature) {
-					
+					while (condition_feature) {
 						System.out.print("Enter slang want to edit: ");
 						String slang_edit = sc.nextLine();
-						ulti.editSlangWord(data,slang_edit);
-						//Exit condition
-						if(ulti.exit("Edit a Slang-Word")) {
+						ulti.editSlangWord(data, slang_edit);
+						// Exit condition
+						if (ulti.exit("Edit a Slang-Word")) {
 							condition_feature = false;
 						}
 						ulti.WriteFileData(data);
 					}
 					break;
-				
-				case "6"://delete slang
+
+				case "6":// delete slang
 					System.out.println("------------------Delete a Slang-Word-------------------------------");
-					while(condition_feature) {
-					
+					while (condition_feature) {
+
 						ulti.deleteSlang(data);
-						//Exit condition
-						if(ulti.exit("Delete a Slang-Word")) {
+						// Exit condition
+						if (ulti.exit("Delete a Slang-Word")) {
 							condition_feature = false;
 						}
 						ulti.WriteFileData(data);
 					}
 					break;
-				case "7"://reset data
+				case "7":// reset data
 					System.out.println("------------------Reset all data------------------------------");
-					while(condition_feature) {
-					
+					while (condition_feature) {
 						String fileNameOriginal = "./src/data/Slag_Original.txt";
-						
+
 						data = ulti.ReadFileData(fileNameOriginal);
-						//Exit condition
-						if(ulti.exit("Reset all data")) {
+						// Exit condition
+						if (ulti.exit("Reset all data")) {
 							condition_feature = false;
 						}
 						ulti.WriteFileData(data);
 					}
 					break;
-				case "8"://random 1 slang-word
+				case "8":// random 1 slang-word
 					System.out.println("------------------Random a Slang-Word------------------------------");
-					while(condition_feature) {
-					
+					while (condition_feature) {
+
 						String random_slang = ulti.randomSlang(data);
 						String[] DefRanSlang = ulti.findDefinition(data, random_slang);
-						if(DefRanSlang.length>0) {
-							System.out.println("Random Slang: \""+random_slang +  "\" ");
+						if (DefRanSlang.length > 0) {
+							System.out.println("Random Slang: \"" + random_slang + "\" ");
 							System.out.print("Definition: ");
-							for(int i=0; i<DefRanSlang.length; i++) {
-								System.out.print(DefRanSlang[i]+"\t");
+							for (int i = 0; i < DefRanSlang.length; i++) {
+								System.out.print(DefRanSlang[i] + "\t");
 							}
 						}
-						//Exit condition
-						if(ulti.exit("Random a Slang-Word")) {
+						// Exit condition
+						if (ulti.exit("Random a Slang-Word")) {
 							condition_feature = false;
 						}
 					}
 					break;
 				case "9":
-					System.out.println("------------------Funny Question: Show a random Slang-word ------------------------------");
-					while(condition_feature) {
-					
+					System.out.println(
+							"------------------Funny Question: Show a random Slang-word ------------------------------");
+					while (condition_feature) {
+
 						ulti.questionFunny_ChooseDef(data);
-					
-						//Exit condition
-						if(ulti.exit("Funny Question: Show a random Slang-word")) {
-							condition_feature = false;
-						}
-					}	
-					break;
-				case "10":
-					System.out.println("------------------Funny Question: Show a random Definition-------------------------------");
-					while(condition_feature) {
-					
-					
-						ulti.questionFunny_ChooseSlang(data);
-						//Exit condition
-						if(ulti.exit("Funny Question: Show a random Definition")) {
+
+						// Exit condition
+						if (ulti.exit("Funny Question: Show a random Slang-word")) {
 							condition_feature = false;
 						}
 					}
 					break;
-				case "11"://exit
+				case "10":
+					System.out.println(
+							"------------------Funny Question: Show a random Definition-------------------------------");
+					while (condition_feature) {
+
+						ulti.questionFunny_ChooseSlang(data);
+						// Exit condition
+						if (ulti.exit("Funny Question: Show a random Definition")) {
+							condition_feature = false;
+						}
+					}
+					break;
+				case "11":// exit
 					condition = false;
 					ulti.WriteFileData(data);
 					break;
 				default:
 					System.out.println("Wrong Input!!!");
 					break;
-		}
-		
-	
-			if(!condition)
+			}
+
+			if (!condition)
 				sc.close();
-		}	
-		
-		
-		
+		}
+
 	}
 }
